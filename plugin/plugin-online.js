9944422cbc160010249826dfe21304e7cd3ea203
chovrio <1480733667@qq.com>
2022-10-16 21:10:04 +0800

    feat: ?

--- plugin/plugin-online.js
+++ plugin/plugin-online.js
@@ -1,11 +1,13 @@
 "use strict"
 const { bot } = require("../index")
 
-bot.on("system.online", function (group) {
-	bot.sendGroupMsg(group, '蒸汽人已上线')
-	// 你的账号已上线，你可以做任何事
-	console.log(`来自plugin-online: 我是${this.nickname}(${this.uin})，我有${this.fl.size}个好友，${this.gl.size}个群`)
-})
-bot.on("system.offline", () => {
-	bot.sendGroupMsg(group, '蒸汽人下线维修去了')
-})
+module.exports = (group) => {
+	bot.on("system.online", function () {
+		bot.sendGroupMsg(group, '蒸汽人已上线')
+		// 你的账号已上线，你可以做任何事
+		console.log(`来自plugin-online: 我是${this.nickname}(${this.uin})，我有${this.fl.size}个好友，${this.gl.size}个群`)
+	})
+	bot.on("system.offline", () => {
+		bot.sendGroupMsg(group, '蒸汽人下线维修去了')
+	})
+}
