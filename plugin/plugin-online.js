12923872b2422b6c21ca3b7f51cd1ddddf50f5c9
chovrio <1480733667@qq.com>
2022-10-17 00:05:46 +0800

    feat: 关闭上线提醒

--- plugin/plugin-online.js
+++ plugin/plugin-online.js
@@ -3,7 +3,7 @@ const { bot } = require("../index")
 
 module.exports = (group) => {
 	bot.on("system.online", function () {
-		bot.sendGroupMsg(group, '蒸汽人已上线')
+		// bot.sendGroupMsg(group, '蒸汽人已上线')
 		// 你的账号已上线，你可以做任何事
 		console.log(`来自plugin-online: 我是${this.nickname}(${this.uin})，我有${this.fl.size}个好友，${this.gl.size}个群`)
 	})
