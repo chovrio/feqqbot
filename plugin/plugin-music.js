5e5d4a91970da1282e10a0327f0e9924df918877
chovrio <1480733667@qq.com>
2022-10-20 10:27:44 +0800

    test: testr

--- plugin/plugin-music.js
+++ plugin/plugin-music.js
@@ -1,14 +1,14 @@
 const https = require('https')
+const url = 'https://api.vvhan.com/api/reping'
 module.exports = function music(msg) {
     if (msg.atme) {
         let text;
-        // console.log(msg);
         msg.message.map((item) => {
             if (item.type == 'text') {
                 text = item.text.trim()
             }
         })
-        https.get('https://api.vvhan.com/api/reping', (res) => {
+        https.get(url, (res) => {
             let list = [];
             res.on('data', (data) => {
                 list.push(data)
@@ -22,26 +22,5 @@ module.exports = function music(msg) {
             console.log('Error: ' + err.message);
             msg.reply('啊哦，菲菲不会了')
         })
-        //console.log(text);
-        // 点歌功能
-        /* if (text == '点歌') {
-            msg.reply("点歌正确姿势：点歌+歌名")
-        } else {
-            if (/点歌/.test(text)) {
-                let song = text.trim().replace("点歌", '').replace("@蒸汽人", "")
-                console.log(`https://api.vvhan.com/api/music?id=${song}&type=song&media=netease`);
-                https.get(`https://api.vvhan.com/api/music?id=${song}&type=song&media=netease`, res => {
-                    let list = [];
-                    res.on('data', (data) => {
-                        console.log(data);
-                        list.push(data)
-                    })
-                    res.on('end', async () => {
-                        let data = await JSON.parse(Buffer.concat(list).toString())
-                        console.log(data);
-                    })
-                })
-            }
-        } */
     }
 }
